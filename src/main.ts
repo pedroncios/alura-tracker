6e18d7328923d678b76c3b6b8c8ca48920aca732
Pedro Bondal <pedroyamada@gmail.com>
2023-09-27 19:00:39 -0300

    Configuração FontAwesome

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
+// Vue
 import { createApp } from 'vue'
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faPlay, faStop } from '@fortawesome/free-solid-svg-icons';
+// Components
 import App from './App.vue'
 
-const app = createApp(App)
+library.add(faPlay, faStop);
 
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app')
