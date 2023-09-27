40980d0baed63e71aac1f4d162b16aa6898699db
Pedro Bondal <pedroyamada@gmail.com>
2023-09-27 19:01:32 -0300

    Base do componente para criação de nova tarefa

---
+++ src/components/TarefaForm.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TarefaForm'
+})
+</script>
+
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" />
+      </div>
+      <div class="column">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <section>
+            <strong>00:00:00</strong>
+          </section>
+          <button class="button">
+            <span class="icon">
+              <font-awesome-icon icon="fa-solid fa-play" />
+            </span>
+            <span>play</span>
+          </button>
+          <button class="button">
+            <span class="icon">
+              <font-awesome-icon icon="fa-solid fa-stop" />
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
