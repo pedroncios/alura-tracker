f392fe8b0d33e5677ee727606f84b76f12d84b36
Pedro Bondal <pedroyamada@gmail.com>
2023-09-27 17:44:45 -0300

    Projeto inicial com componente de barra lateral

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import BarraLateral from './components/BarraLateral.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: { BarraLateral }
+})
+</script>
+
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter"></div>
+  </main>
+</template>
+
+<style scoped></style>
